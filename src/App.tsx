41399c87a79ea50bdcedaf9413d12f667516c504
grigorisem <grigorisem07@gmail.com>
2025-07-21 20:17:36 +0500

    grid 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,17 @@ import { Goods } from './components/Goods'
 import slide1 from './assets/img/slide1.jpg'
 import slide2 from './assets/img/slide2.jpg'
 import slide3 from './assets/img/slide3.webp'
+import slide4 from './assets/img/converse.webp'
+import slide5 from './assets/img/rickowens.webp'
+import slide6 from './assets/img/kobe6grinch.jpg'
+
 const slides = [
     slide1, 
     slide2,
     slide3,
+    slide4,
+    slide5,
+    slide6,
 ];
 
 function App() {
@@ -21,8 +28,9 @@ function App() {
       <Slider>
         {
           slides.map(
-            (s) => (
+            (s, index) => (
               <img 
+                key={`slides-${index}`}
                 src={s}
                 className="w-full h-full object-center rounded-2xl max-h-[700px] object-contain"
               />
@@ -33,10 +41,11 @@ function App() {
       <Goods>
         {
           slides.map(
-              (s) => (
+              (s, index) => (
                 <img 
+                  key={`goods-${index}`}
                   src={s}
-                  className="w-full h-full object-center rounded-2xl max-h-[700px] object-contain"
+                  className="w-full h-full object-center rounded-2xl max-h-[500px] object-cover"
                 />
               )
             )
--- src/components/Goods.tsx
+++ src/components/Goods.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, Children } from "react";
+import { Children } from "react";
 import type { ReactNode } from "react";
 
 interface GoodsProps {
@@ -8,6 +8,15 @@ export const Goods = ({children}: GoodsProps ) => {
     const goodsArray = Children.toArray(children);
     return (
         <>
+          <div className="grid grid-cols-3 gap-[10px] justify-items-center">
+              {goodsArray.map((good, index) => (
+                <div className="card max-w-[400px] max-h-[500px] w-full h-full" key={index}>
+                    {good}
+                </div>
+                )
+              )
+            }
+          </div>
         </>
     )
 }
